372729103540050cf9351de085d9d8174e40c9b4
Bulashev Nikolay <nvbulashev@gmail.com>
2018-03-31 14:09:15 +0500

    terraform initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
- 
+provider "google" {
+ version = "1.4.0"
+ project = "mydrive-1232"
+ region = "europe-west1"
+}
+resource "google_compute_instance" "app" {
+  name = "reddit-app-terra"
+  machine_type = "g1-small"
+  zone = "europe-west1-b"
+  boot_disk {
+    initialize_params {
+      image = "reddit-full"
+    }
+  }
+  network_interface {
+  network = "default"
+  access_config {}
+  }
+}
